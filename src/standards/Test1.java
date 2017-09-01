95b58f28ed11f4dcb3d099e0f6d476393d5a9f0a
iDubTheniStep <dp201524@students.davenport.k12.ia.us>
2017-09-01 08:29:59 -0500

    Assessment-1
    :100:

---
+++ src/standards/Test1.java
@@ -0,0 +1,11 @@
+package standards;
+
+import javax.swing.JOptionPane;
+
+public class Test1
+{
+	 public static void main(String[] args)
+	 {
+		 JOptionPane.showMessageDialog (null, "Say something funny");
+	 }
+}
---
+++ src/standards/Test3.java
@@ -0,0 +1,13 @@
+package standards;
+
+import javax.swing.JOptionPane;
+
+public class Test3
+{
+	 public static void main(String[] args)
+	 {
+		 JOptionPane.showMessageDialog(null, "Hello world");
+		 //System.out.println("Hello world");
+	 }
+
+}
